eff8c5b873744ae173d4e28038969a6fb5051a85
KJ <kristoff125@gmail.com>
2021-04-03 18:38:15 -0400

    final touches

--- story.py
+++ story.py
@@ -1,11 +1,11 @@
 import random
 stories = [
-    ["Pizza thief committed at local Pizza Hut", "The Pizza thief was never found..."],
-    ["Lost paperclip reported in downtown", "The lost paperclip was never found..."],
-    ["$GME shares for free in wall st.", "You never found free shares of $GME..."],
+    ["Pizza thief reported at local Pizza Hut", "The Pizza thief was never found..."],
+    ["Billionaire lost favorite paperclip reported in downtown", "The lost paperclip was never found..."],
+    ["$GME shares for free on Wall St.", "$GME crashed, no diamond hands here..."],
     ["Duck seen flying towards the reds", "The suspicious Duck was never found..."],
     ["Your co-worker has stolen your donut", "The lost donut was never found..."],
-    ["Your girlfriend has been seen with someone", "The girlfriend left u..."]
+    ["Your girlfriend has been seen with someone", "She left you years ago..."]
 ]
 
 
